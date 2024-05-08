c14ef9e347edf7a7813ccb5ad79461089d1ab75f
ATLP <atlp@ATLPs-MacBook-Pro.local>
2024-05-08 11:32:33 +0200

    test with jest

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -2,9 +2,8 @@ import { render } from "@testing-library/react-native";
 import App from "../app/index";
 
 describe("Render <App />", () => {
-  it("should render the App component", () => {
+  test('should render the App component', () => {
     const { getByText } = render(<App />);
-
-    expect(getByText("Medica App")).toBeTruthy();
+    expect(getByText("Wellcome to medica app")).toBeTruthy();
   });
 });
