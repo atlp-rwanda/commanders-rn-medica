7fea8d9d78c4001767df437982eee1c82149b1ff
justinmihigo <mihigojustin28@gmail.com>
2024-05-09 22:30:06 +0200

    squash commits
    squash commits
    
    squash commits
    
    squash commits
    
    fix(rendering error)
    add a new date picker with react-native-modal-datetime-picker library
    
    fix(rendering error)
    add a new date picker with react-native-modal-datetime-picker library

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,36 +2,29 @@
 module.exports = {
   content: [
     "./screens/**/*.{js,ts,jsx,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+     "./app/**/*.{js,ts,jsx,tsx}",
+     "./components/**/*.{js,ts,jsx,tsx}",
+     "./App.{js,jsx,ts,tsx}",
+],
   theme: {
-    extend: {
-      colors: {
-        "primary-500": "#246BFD",
-        "primary-400": "#5089FD",
-        "primary-300": "#7CA6FE",
-        "primary-200": "#A7C4FE",
-        "primary-100": "#E9F0FF",
-        "Greyscale/900": "#212121",
-        "Greyscale/600": "#757575",
-        "Greyscale/400": "#BDBDBD",
-        "Greyscale/100": "#F5F5F5",
-      },
+    extend: {colors: {
+      lightblue:"#246BFD",
+      darkblue:"#3062C8",
+      lightgrey:"#FAFAFA",
+      grey:"#9E9E9E",
+      white:"#FFFFFF",
+      def:"#FFFFFF"
     },
-    fontWeight: {
-      thin: "100",
-      hairline: "100",
-      extralight: "200",
-      light: "300",
-      normal: "400",
-      medium: "500",
-      semibold: "600",
-      bold: "700",
-      extrabold: "800",
-      "extra-bold": "800",
-      black: "900",
+    fontFamily: {
+      UrbanistRegular: ["UrbanistRegular", "sans-serif"],
+      UrbanistSemiBold:["UrbanistSemiBold", "sans-serif"],
+      UrbanistBold:["UrbanistBold", "sans-serif"],
+      UrbanistMedium:["UrbanistMedium", "sans-serif"],
     },
   },
+    
+  },
   plugins: [],
-};
+}
+
+
