6b42f37bf898e040b4f3ce24c37ce0d2f3cc3828
Kibongo Simon Peter <kibongos40@gmail.com>
2024-05-10 14:35:56 +0200

    Updated splash picture, logo and favicon, Added onboarding screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,31 +1,10 @@
 import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { Pressable, StyleSheet, Text, View } from "react-native";
+import OnBoarding from "./onboarding/OnBoarding";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Text>Medica App</Text>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Pressable>
-        <Link href="/(tabs)">Tabs</Link>
-        <Link href="/counter/Counter">
-          <Text>Click me to test redux</Text>
-        </Link>
-      </Pressable>
-      <Link href="/reset-password/" style={{ marginTop: 20 }}>
-        Forgot Password
-      </Link>
-    </View>
+    <OnBoarding />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
