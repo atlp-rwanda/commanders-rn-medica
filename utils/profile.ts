4d54f9dfec6c92781157360aaafb36ddcb587dcc
latiah <kimtifah2@gmail.com>
2024-06-17 02:41:59 +0200

    fix:onboarding screens
    forgot password with phone
    
    phone forgot
    
    sending verfication code
    
    send otp sms verficstion
    
    feat:(resetting password with phone)

--- utils/profile.ts
+++ utils/profile.ts
@@ -1,6 +1,6 @@
 import { supabase } from "@/app/supabase";
 
-export const getImage = async ({
+export const getProfileImage = async ({
 	bucket,
 	fileName,
 }: {
@@ -9,10 +9,19 @@ export const getImage = async ({
 }): Promise<string | undefined> => {
 	const { error, data } = await supabase.storage
 		.from(bucket)
-		.createSignedUrl(fileName, 3600, { download: false });
+		.createSignedUrl(
+			fileName.startsWith("public/")
+				? fileName
+				: fileName.split("/files/")[1],
+			3600,
+			{ download: false }
+		);
 
 	if (error) {
-		console.error("Error generating signed URL:", error);
+		console.error(
+			"Error generating signed URL:",
+			JSON.stringify(error, null, 2)
+		);
 		return undefined;
 	}
 
