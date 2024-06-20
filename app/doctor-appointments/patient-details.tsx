30c73e549b33dea0182133fe9799fce7c9a3b3d1
latiah <kimtifah2@gmail.com>
2024-06-20 22:54:16 +0200

    feat:book appointment implementation

--- app/doctor-appointments/patient-details.tsx
+++ app/doctor-appointments/patient-details.tsx
@@ -3,7 +3,7 @@ import { NavigationHeader } from "@/components/NavigationHeader";
 import { Text } from "@/components/ThemedText";
 import { Select } from "@/components/select";
 import { router } from "expo-router";
-import { useState } from "react";
+import { useState , useEffect} from "react";
 import {
   KeyboardAvoidingView,
   ScrollView,
@@ -11,13 +11,47 @@ import {
   View,
   useWindowDimensions,
 } from "react-native";
+import { supabase } from "../supabase"; 
 
 export default function PatientDetailsScreen() {
   const dimensions = useWindowDimensions();
-  const [names, setNames] = useState("Andrew Ainsley");
-  const [gender, setGender] = useState("1");
-  const [age, setAge] = useState("25");
-  const [problem, setProblem] = useState("Hello Dr. Jenny, I have a problem with my immune system. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.");
+  const [names, setNames] = useState("");
+  const [gender, setGender] = useState("");
+  const [age, setAge] = useState("");
+  const [problem, setProblem] = useState("");
+  const [error, setFetchError]=useState<string | null>(null);
+  const [user, setUser] = useState<any | null>(null);
+  const[patientDetails, setPatientDetails]=useState<any| null>(null);
+  
+  useEffect(() => {
+    const fetchPatientDetails = async () => {
+      const { data: userData, error: userError } = await supabase.auth.getUser();
+      if (userError) {
+        setFetchError(userError.message);
+        return;
+      }
+      const userId = userData?.user?.id;
+      if (userId) {
+        const { data: patientData, error: patientError } = await supabase
+          .from("patient")
+          .select("*")
+          .eq("id", userId)
+          .single();
+        if (patientError) {
+          setFetchError(patientError.message);
+        } else {
+          setUser(userData.user);
+          setPatientDetails(patientData);
+          setNames(patientData.full_name);
+          setGender(patientData.gender);
+          setAge(patientData.age);
+          setProblem(patientData.problem)
+        }
+      }
+    };
+    fetchPatientDetails();
+  }, []);
+
 
   const genders = [
     { key: 1, value: "Male" },
@@ -28,29 +62,45 @@ export default function PatientDetailsScreen() {
     return { key: i, value: i + 1 + " years" }
   });
 
+
   return (
     <View className="px-5 flex-1">
       <NavigationHeader title="Patient Details" />
       <ScrollView className="flex-1" showsVerticalScrollIndicator={false}>
+    
         <KeyboardAvoidingView
           behavior="padding"
           className="flex-1"
           style={{ maxHeight: dimensions.height, }}
         >
+         
+            <View>
           <View className="mt-4 mb-3">
             <Text className="text-lg font-UrbanistBold mb-2">Full Name</Text>
+           
             <TextInput placeholder="Enter full name"
               value={names}
               onChangeText={setNames}
             />
+            
           </View>
           <View className="mb-5">
             <Text className="text-lg font-UrbanistBold mb-2">Gender</Text>
-            <Select data={genders} setSelected={setGender} defaultOption={genders[0]} />
+            <Select
+                data={genders}
+                setSelected={setGender}
+                defaultOption={genders.find((g) => g.value === gender)}
+              />
           </View>
           <View className="mb-5">
             <Text className="text-lg font-UrbanistBold mb-2">Your Age</Text>
-            <Select data={ageArray} setSelected={setAge} defaultOption={ageArray[24]} maxHeight={150} />
+            <Select
+                data={ageArray}
+                setSelected={setAge}
+                defaultOption={ageArray.find((a) => a.value === age + " years")}
+                maxHeight={150}
+              />
+            
           </View>
           <View className="mb-5">
             <Text className="text-lg font-UrbanistBold mb-2">
@@ -64,14 +114,15 @@ export default function PatientDetailsScreen() {
               numberOfLines={5}
               className="h-52"
             />
-          </View>
+              </View>
+            </View>
         </KeyboardAvoidingView>
       </ScrollView>
       <View className="py-3">
         <TouchableOpacity
           className="bg-primary-500 p-4 rounded-full items-center"
           onPress={() => {
-            router.push("/doctor-appointments/payments")
+            router.push("/doctor-appointments/review-summary")
           }}
         >
           <Text className="text-white font-UrbanistBold">Next</Text>
